935c7ba06698ba1a9d50c41169056bdb8a432343
satyam mishra <satyam.growupnext@gmail.com>
2022-06-18 16:34:29 +0530

    second leadcture completed

--- 01.js
+++ 01.js
@@ -1,8 +1,45 @@
-// console.log('hello world');
-"use strict";
+console.log('hello world');
 test(10, 20);
 
 function test(a, b) {
     add = a + b;
     console.log(a + b);
 }
+console.log(Math.pow(8, 1 / 3));
+
+console.log(Math.max(4, 19, 28, 18, 10));
+console.log(Math.random());
+
+console.log(Math.cbrt(216));
+console.log(Math.hypot(45, 987));
+
+console.log(Math.clz32(0xf));
+
+console.log(4 / 0);
+
+
+console.log(Math.trunc(Math.sin(0)));
+
+
+
+function area(a) {
+    let A = (Math.sqrt(3)) / 4;
+    let B = A * Math.pow(a, 2)
+    console.log(Math.round(B));
+}
+
+area(5);
+
+console.log(Number.isNaN("2"));
+
+console.log(Number.parseInt("3z"));
+
+let A = 0.3 - 0.2;
+let B = 0.2 - 0.1;
+
+console.log(A);
+console.log(B);
+
+let googol = "1" + "0".repeat(100);
+
+console.log(BigInt(googol));
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # JS-Assignment
-This is the JS assignments repo made  by me with the instructions from HITESH CHAUDHARY try to post all the assignments given by him on dailt basis.
+This is the JS assignments repo made  by me with the instructions from HITESH CHAUDHARY try to post all the assignments given by him on daily basis.
